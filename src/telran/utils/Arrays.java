27abb61e8b8398e421b6d1b30bcddad2b9cfddfc
user.emial <emil.mehraliyev003@gmail.com>
2024-03-30 20:54:13 +0300

    HW11 BinarySearch

---
+++ src/telran/utils/Arrays.java
@@ -0,0 +1,85 @@
+package telran.utils;
+
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public class Arrays {
+	public static <T> int indexOf(T[] array, T element) {
+		int index = 0;
+		while (index < array.length && !equals(array[index], element)) {
+			index++;
+		}
+		return index == array.length ? -1 : index;
+	}
+
+	private static <T> boolean equals(T elem1, T elem2) {
+
+		return elem1 == null ? elem1 == elem2 : elem1.equals(elem2);
+	}
+
+	public static <T> T min(T[] array, Comparator<T> comp) {
+		T res = null;
+		if (array != null && array.length > 0) {
+			res = array[0];
+			for (int i = 1; i < array.length; i++) {
+				if (comp.compare(res, array[i]) > 0) {
+					res = array[i];
+				}
+			}
+		}
+		return res;
+
+	}
+
+	public static <T> void bubbleSort(T[] array, Comparator<T> comp) {
+		int n = array.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (comp.compare(array[j], array[j + 1]) > 0) {
+					T temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public static <T> int binarySearch(T[] array, T key, Comparator<T> comp) {
+
+		int left = 0;
+		int right = array.length - 1;
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			int cmp = comp.compare(array[middle], key);
+			if (cmp < 0) {
+				left = middle + 1;
+			} else if (cmp > 0) {
+				right = middle - 1;
+			} else {
+				return middle;
+			}
+		}
+
+		return -1;
+	}
+
+	public static <T> T[] removeIf(T[] array, Predicate<T> predicate) {
+		// TODO
+		// removes all elements of array matching a given predicate
+		return null;
+	}
+
+	public static <T> T[] search(T[] array, Predicate<T> predicate) {
+		// Impossible to allocate memory for generic array
+		// Only Arrays.copyOf may be used
+		T[] arResult = java.util.Arrays.copyOf(array, array.length);
+		int index = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (predicate.test(array[i])) {
+				arResult[index++] = array[i];
+			}
+		}
+		return java.util.Arrays.copyOf(arResult, index);
+	}
+
+}
---
+++ src/telran/utils/test/ArraysTest.java
@@ -0,0 +1,86 @@
+
+package telran.utils.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Comparator;
+
+import org.junit.jupiter.api.Test;
+
+import telran.utils.Arrays;
+
+class ArraysTests {
+Integer[] numbers = {100, -3, 23, 4, 8, 41, 56, -7};
+String[] strings = {"abc", "lmn", "123", null, "a"};
+String[] stringsMin = {"abc", "lmn", "123",  "y"};
+	@Test
+	void indexOfTest() {
+		assertEquals(1, Arrays.indexOf(strings, "lmn"));
+		assertEquals(3, Arrays.indexOf(strings, null));
+		assertEquals(-1, Arrays.indexOf(numbers, 150));
+		assertEquals(4, Arrays.indexOf(numbers, 8));
+	}
+	@Test
+	void minValueTest() {
+		Comparator<String> compLength = (s1, s2) -> s1.length() - s2.length();
+		assertEquals("y", Arrays.min(stringsMin,
+				compLength));
+		Comparator<String> compNative = (s1, s2) -> s1.compareTo(s2);
+		assertEquals("123", Arrays.min(stringsMin, compNative));
+	}
+	@Test
+	void bubbleSortTest() {
+		Integer [] expected = {4, 8, 56, 100, 41, 23, -3, -7};
+		Integer [] numbersCopy = java.util.Arrays.copyOf(numbers, numbers.length);
+         /* lambda expression example */
+	//	Comparator<Integer> evenOddComp = (num1, num2) ->
+   //	evenOddComparator(num1, num2);
+		/*************************************/
+		/* method reference */
+		Comparator<Integer> evenOddComp = ArraysTests::evenOddComparator;
+		/*********************************************/
+		Arrays.bubbleSort(numbersCopy, evenOddComp);
+		assertArrayEquals(expected, numbersCopy);
+		
+	}
+	static int evenOddComparator(Integer num1, Integer num2) {
+		
+		boolean isNum1Even = num1 % 2 == 0;
+		boolean isNum2Even = num2 % 2 == 0;
+		int res = 1;
+		if (isNum1Even && isNum2Even) {
+			res = Integer.compare(num1, num2);
+		} else if (!isNum1Even && !isNum2Even) {
+			res = Integer.compare(num2, num1);
+		} else if (isNum1Even) {
+			res = -1;
+		}
+		return res;
+	}
+	@Test
+	void searchTest() {
+		Integer[] expectedEven = {100, 4, 8,  56};
+		assertArrayEquals(expectedEven, Arrays.search(numbers,
+				a -> a % 2 == 0));
+		Integer[] expectedNegative = {-3,-7};
+		assertArrayEquals(expectedNegative, Arrays.search(numbers,
+				a -> a < 0));
+		
+	}
+	@Test
+	void binarySearchTest() {
+		Integer [] array = {1,3,5,7,9,11,13,15,17,19,24,35};
+		int key = 15;
+		Comparator<Integer> comp = Comparator.naturalOrder();
+		int result = Arrays.binarySearch(array, key, comp);
+		assertEquals(7,result);
+		key = 20;
+		result = Arrays.binarySearch(array, key, comp);
+		assertEquals(-1,result);
+	}
+	 @Test
+	 void removeIfTest() {
+		 //TODO
+	 }
+
+}
